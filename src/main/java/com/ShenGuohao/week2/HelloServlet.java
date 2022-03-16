5cf85c8c665ae440508f1ec5b2ac738bb661f24d
shenguohao123 <88016502+shenguohao123@users.noreply.github.com>
2022-03-16 11:28:31 +0800

    week2

---
+++ src/main/java/com/ShenGuohao/week2/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.ShenGuohao.week2;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", value = "/HelloServlet")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Name: ShenGuohao");
+        writer.println("ID: 2020211001001330");
+        writer.println("Date and Time :Sun, 06 Mar 2022 11:30:40 GMT");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
